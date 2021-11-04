19c4d02e8219a3a2b66dccd91bbd5873016bf972
TrevorSlobodnick <trevorslobodnick@gmail.com>
2021-11-04 12:06:45 -0400

    testing

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TrevorsBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
